40f0191a9d5e9da776b33d06fe860803a03aaae6
javiviviviv <jai.flores@duocuc.cl>
2025-05-22 09:54:59 -0400

    no constructores ni getters y setters por el lombok

--- src/main/java/com/ecomarketic/project/ecomarketic/model/Cliente.java
+++ src/main/java/com/ecomarketic/project/ecomarketic/model/Cliente.java
@@ -30,62 +30,6 @@ public class Cliente {
     @Column(nullable = false)
     private String direccion;
 
-    public Cliente(Integer id, String nombre, String apellido, Integer n_telefono, String mail, String direccion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.n_telefono = n_telefono;
-        this.mail = mail;
-        this.direccion = direccion;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Integer getN_telefono() {
-        return n_telefono;
-    }
-
-    public void setN_telefono(Integer n_telefono) {
-        this.n_telefono = n_telefono;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
 }
 
 
--- src/main/java/com/ecomarketic/project/ecomarketic/model/Producto.java
+++ src/main/java/com/ecomarketic/project/ecomarketic/model/Producto.java
@@ -33,62 +33,6 @@ public class Producto {
     @Column(nullable = false)
     private Integer precio;
 
-    public Producto(Integer id, String nombre, String descripcion, String categoria, Integer stock, Integer precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.categoria = categoria;
-        this.stock = stock;
-        this.precio = precio;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public Integer getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Integer precio) {
-        this.precio = precio;
-    }
 }
 
 
