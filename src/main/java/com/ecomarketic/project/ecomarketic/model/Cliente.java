1a57d269130bd1b0188b468a6e2d4382deae4f74
javiviviviv <jai.flores@duocuc.cl>
2025-05-22 02:36:22 -0400

    clases cliente y producto parte 2

--- src/main/java/com/ecomarketic/project/ecomarketic/model/Cliente.java
+++ src/main/java/com/ecomarketic/project/ecomarketic/model/Cliente.java
@@ -1,11 +1,38 @@
 package com.ecomarketic.project.ecomarketic.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+
+@Table(name= "cliente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(nullable = false)
     private String nombre;
-    private String apellido;
-    private int n_telefono;
-    private String mail;
-    private String direccion;
 
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(length = 9, nullable = false)
+    private Integer n_telefono;
+
+    @Column(nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String direccion;
 }
+
+
+
--- src/main/java/com/ecomarketic/project/ecomarketic/model/Producto.java
+++ src/main/java/com/ecomarketic/project/ecomarketic/model/Producto.java
@@ -1,13 +1,39 @@
 package com.ecomarketic.project.ecomarketic.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+
+@Table(name= "producto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(nullable = false)
     private String nombre;
-    private int id;
-    private String categoria;
-    private String descripcion;
-    private int precio;
-    private int stock;
 
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Integer precio;
 
 }
+
+
+
