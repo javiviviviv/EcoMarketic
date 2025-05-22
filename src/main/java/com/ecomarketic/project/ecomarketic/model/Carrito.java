c2045b926aa040af345fd7277fe5d1674d0f5ab5
javiviviviv <jai.flores@duocuc.cl>
2025-05-22 03:05:24 -0400

    carritochamo

--- src/main/java/com/ecomarketic/project/ecomarketic/model/Carrito.java
+++ src/main/java/com/ecomarketic/project/ecomarketic/model/Carrito.java
@@ -1,37 +1,32 @@
 package com.ecomarketic.project.ecomarketic.model;
 
+import com.ecomarketic.project.ecomarketic.model.*;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Date;
 
 @Entity
 
-@Table(name= "cliente")
+@Table(name= "carrito")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Cliente {
+public class Carrito {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
-    private String nombre;
+//arraylist productos
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
 
     @Column(nullable = false)
-    private String apellido;
+    private String estado;
 
-    @Column(length = 9, nullable = false)
-    private Integer n_telefono;
-
-    @Column(nullable = false)
-    private String mail;
-
-    @Column(nullable = false)
-    private String direccion;
+    private Date fecha;
 }
-
-
-
